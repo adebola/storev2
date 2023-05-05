b143bd1e27c03e317ed4cda44dcdce3c63f35f4b
Adebola Omoboya <adeomoboya@gmail.com>
2023-05-05 07:30:11 +0100

    First Commit

---
+++ src/main/java/io/factorialsystems/storeproducts/StoreProductsApplication.java
@@ -0,0 +1,13 @@
+package io.factorialsystems.storeproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreProductsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/factorialsystems/storeproducts/StoreProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.factorialsystems.storeproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
